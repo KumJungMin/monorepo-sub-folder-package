e38ee77249cb4f8a091b1b8d427b446b58d47132
KumJungMin <rmawjdals@naver.com>
2023-05-14 15:54:07 +0900

    chore: 폴더를 패키지화시키기(gc/ui, gc/shares)

---
+++ packages/components/shares/index.js
@@ -0,0 +1,3 @@
+import MenuHeader from "./MenuHeader.vue";
+
+export { MenuHeader };
--- packages/service/src/App.vue
+++ packages/service/src/App.vue
@@ -1,5 +1,30 @@
+<template>
+  <div>
+    <MenuHeader :menu-items="MENU_ITEMS" />
+    <Checkbox />
+  </div>
+</template>
+
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
-</script>
+import { MenuHeader } from "@gc/shares";
+import { Checkbox } from "@gc/ui";
 
-<template></template>
+const MENU_ITEMS = [
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    label: "About",
+    link: "/about",
+  },
+  {
+    id: 3,
+    label: "Contact",
+    link: "/contact",
+  },
+];
+</script>
